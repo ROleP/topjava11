bf9e5f2fd48c7d38121c0999b1befc6765718570
ROleP <crazy.rolep@gmail.com>
2017-09-14 19:48:27 +0300

    2_6_annotation_processing

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -4,13 +4,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.repository.UserRepository;
-import ru.javawebinar.topjava.service.UserService;
+import ru.javawebinar.topjava.web.user.AdminRestController;
 
 import java.util.Arrays;
 
 public class SpringMain {
   public static void main(String[] args) {
+/*
     ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
     System.out.println("Bean defifnition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
 //    UserRepository userRepository = (UserRepository) appCtx.getBean("mockUserRepository");
@@ -21,5 +21,12 @@ public class SpringMain {
     userRepository.save(new User(null, "userName", "email", "password", Role.ROLE_ADMIN));
 
     appCtx.close();
+    */
+
+    try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
+      System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+      AdminRestController adminRestController = appCtx.getBean(AdminRestController.class);
+      adminRestController.create(new User(null, "username", "email", "password", Role.ROLE_ADMIN));
+    }
   }
 }
