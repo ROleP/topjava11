769336c043ae1b5a28ae6de27b7d4d68f22b3b63
ROleP <crazy.rolep@gmail.com>
2017-09-14 17:34:03 +0300

    2_3_app_layers

---
+++ src/main/java/ru/javawebinar/topjava/model/BaseEntity.java
@@ -0,0 +1,29 @@
+package ru.javawebinar.topjava.model;
+
+public class BaseEntity {
+  protected Integer id;
+
+  public BaseEntity() {
+  }
+
+  public BaseEntity(Integer id) {
+    this.id = id;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public boolean isNew() {
+    return (this.id == null);
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Entity %s (%s)", getClass().getName(), getId());
+  }
+}
