f8752caa9cea75486bcfdee99a39ae4ffbcc7379
ROleP <crazy.rolep@gmail.com>
2017-09-19 16:51:11 +0300

    4_6_add_jpa

--- src/main/java/ru/javawebinar/topjava/model/BaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/BaseEntity.java
@@ -1,11 +1,18 @@
 package ru.javawebinar.topjava.model;
 
+import javax.persistence.*;
+
+@MappedSuperclass
+@Access(AccessType.FIELD)
 public class BaseEntity {
   public static final int START_SEQ = 100000;
 
+  @Id
+  @SequenceGenerator(name = "global_seq", sequenceName = "global_seq", allocationSize = 1, initialValue = START_SEQ)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq")
   protected Integer id;
 
-  public BaseEntity() {
+  protected BaseEntity() {
   }
 
   public BaseEntity(Integer id) {
